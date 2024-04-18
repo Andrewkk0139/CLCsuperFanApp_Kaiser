fb1f3a3a0b97f265703a563af65540c34943ffcb
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2024-04-18 09:48:42 -0500

    codes work for sure now. AlertControllrs to be added next instead of labels

--- CLCsuperFanApp_Kaiser/AccessCodeVC.swift
+++ CLCsuperFanApp_Kaiser/AccessCodeVC.swift
@@ -33,49 +33,59 @@ class AccessCodeVC: UIViewController {
         let tempCode = codeFieldOutlet.text ?? "nil"
         var invalidBool = false
         var alrRedBool = false
-        
+        print("Already Redeemed Codes:")
+
+        for i in 0..<AppData.user.usedCodes.count - 1{
+            print(AppData.user.usedCodes[i])
+        }
+        print("end of redeemed list")
         for i in 0..<AppData.masterCodes.count {
             // checks to see if the code is valid
-            print(tempCode)
-            print(AppData.masterCodes[i].code)
+            //print(tempCode)
+            //print(AppData.masterCodes[i].code)
             if AppData.masterCodes[i].code == tempCode {
                 invalidBool = true
                 // if code is valid
                 print("code is valid")
                 let firebaseCode = AppData.masterCodes[i]
+                var foundUsed = false
                 for k in 0..<AppData.user.usedCodes.count {
-                    
                     
                     // code isn't used already by user
                     if AppData.user.usedCodes[k] != tempCode{
                         // checks to see if its went thru whole array
-                        print("value of K:\(k), value of count: \(AppData.user.usedCodes.count - 1)")
-                        if k == AppData.user.usedCodes.count - 1 {
+                        if k == AppData.user.usedCodes.count - 1 && !foundUsed{
                             alrRedBool = true
                             print("Code hasn't been used already")
+                            
+                            // adding points
                             print("*BEFORE REDEEMED* Username:\(AppData.user.username) Points: \(AppData.user.points)")
                             AppData.user.points += firebaseCode.value
                             codeRedOutlet.isHidden = false
                             print("*AFTER REDEEMED* Username:\(AppData.user.username) Points: \(AppData.user.points)")
-                            // updating on FireBase
-                            ref.child("Users").child("\(AppData.user.firebaseKey)").child("points:").setValue(AppData.user.points)
                             AppData.user.usedCodes.append(tempCode)
-                            // updating on FireBase
+
+                            // updating on FireBase for points
+                            ref.child("Users").child("\(AppData.user.firebaseKey)").child("points:").setValue(AppData.user.points)
+                            
+                            // updating on FireBase for codes
                             ref.child("Users").child("\(AppData.user.firebaseKey)").child("usedCodes:").setValue(AppData.user.usedCodes)
+                            
                             // updating leaderboard
                             AppData.masterUsers.sort(by: {$0.points > $1.points})
                             for i in 0..<AppData.masterUsers.count {
                                 AppData.masterUsers[i].declareRank(i)
                             }
+                            
                             // updating lifespan of code
                                 // updates masterCode
                             AppData.masterCodes[i].life -= 1
                                 // updates on firebase
-                            ref.child("Codes").child("\(firebaseCode.firebaseKey)").child("life:").setValue(firebaseCode.life - 1)
-
+                            ref.child("Codes").child("\(firebaseCode.firebaseKey)").child("life:").setValue(firebaseCode.life)
                             return
                         }
                     } else {
+                        foundUsed = true
                         //codeAlrRedeemedOutlet.isHidden = false
                         print("Code has been already redeemed")
                         //break
