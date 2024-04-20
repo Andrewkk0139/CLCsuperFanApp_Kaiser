0d64e9f41b727cf036d2e2dfca8f1368e3dc2c66
stasik <stazilastas@gmail.com>
2024-04-19 21:24:14 -0500

    Register a user account and save it to the Firestore

--- CLCsuperFanApp_Kaiser/SignUp/SignUpViewController.swift
+++ CLCsuperFanApp_Kaiser/SignUp/SignUpViewController.swift
@@ -25,8 +25,8 @@ class SignUpViewController: UIViewController {
         
     }
     
-    func isPasswordValid(password: String) -> Bool{
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&] {8,}")
+    func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
         return passwordTest.evaluate(with: password)
     }
     
@@ -36,7 +36,8 @@ class SignUpViewController: UIViewController {
             return "Please fill in all fields"
         }
         let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespaces)
-        if isPasswordValid(password: cleanedPassword){
+        
+        if isPasswordValid(cleanedPassword){
                 
         }else{
             return "Ensure your password is at least 8 characters, contains a special character and a number"
@@ -51,23 +52,47 @@ class SignUpViewController: UIViewController {
         let error =  validateFields()
         
         if error == nil{
-            Auth.auth().createUser(withEmail: "", password: "") { result, err in
+            
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            Auth.auth().createUser(withEmail: email, password: password) { result, err in
                 //checks for errors
                 if let err = err{
                     //There was an error creating the user
                     print(err)
                 }else{
                     //User was added successfully
+                    //create user
                     let db = Firestore.firestore()
-                    //db.collection("users").addDocument(data: )
+                    db.collection("users").addDocument(data: ["firstName" : firstName, "lastName" : lastName, "uid" : result!.user.uid]) { error in
+                        if error != nil{
+                            print("Error saving user")
+                        }else{
+                            //transition to the home screen
+                            self.transitionToHome()
+                        }
+                    }
                 }
             }
         }else{
             print(error!)
         }
-        //create user
-        //transition to the home screen
+        
+       
+    }
+    
+    func transitionToHome(){
+        if let homeViewController = storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? MainVC{
+            
+            //transition(from: self, to: homeViewController, duration: 1) {
+                
+            //}
+        }
     }
     
 
 }
+
