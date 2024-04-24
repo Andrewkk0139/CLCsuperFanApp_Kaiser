d66bf218fd590fa5d2f37c2821469e98bcbd3c25
Stanislav <stazilastas@gmail.com>
2024-04-24 09:25:25 -0500

    Merged the HomeVC class to LogIn VC

--- CLCsuperFanApp_Kaiser/Authentication/LogInViewController.swift
+++ CLCsuperFanApp_Kaiser/Authentication/LogInViewController.swift
@@ -5,16 +5,65 @@
 //  Created by Stanislav Stajila on 4/19/24.
 //
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 import FirebaseAuth
+
+struct AppData {
+    static var user: Student!
+    static var masterUsers: [Student] = []
+    static var masterCodes: [AccessCode] = []
+    static var count = 0
+}
+
+
 class LogInViewController: UIViewController {
     
     @IBOutlet weak var emailTextFieldOutlet: UITextField!
     @IBOutlet weak var passwordTextFieldOutlet: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     
+    var ref: DatabaseReference!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        
+        // SETTING MASTER STU ARRAY
+        ref.child("Users").observe(.childAdded, with: { (snapshot) in
+            let dict = snapshot.value as! [String:Any]
+            let u = Student(dict: dict)
+            u.firebaseKey = snapshot.key
+            AppData.masterUsers.append(u)
+        })
+        
+        // called after .childAdded
+        ref.child("Users").observeSingleEvent(of: .value, with: { snapshot in
+                print("--inital load has completed and the last user was read--")
+            // sorts
+            AppData.masterUsers.sort(by: {$0.points > $1.points })
+            for i in 0..<AppData.masterUsers.count {
+                AppData.masterUsers[i].declareRank(i)
+                print("Username: \(AppData.masterUsers[i].username) Rank: #\(AppData.masterUsers[i].globalRank)")
+            }
+        })
+        // sorts the array in decending order based on points
+        
+        // SETTING UP MASTER CODE ARRAY
+        ref.child("Codes").observe(.childAdded, with: { (snapshot) in
+            let dict = snapshot.value as! [String:Any]
+            let c = AccessCode(dict: dict)
+            c.firebaseKey = snapshot.key
+            AppData.masterCodes.append(c)
+        })
+        
+        // called after .childAdded
+        ref.child("Codes").observeSingleEvent(of: .value, with: { snapshot in
+                print("--inital load has completed and the last user was read--")
+        })
         
         
     }
--- CLCsuperFanApp_Kaiser/ViewController.swift
+++ CLCsuperFanApp_Kaiser/ViewController.swift
@@ -1,75 +1,75 @@
+////
+////  ViewController.swift
+////  CLCsuperFanApp_Kaiser
+////
+////  Created by ANDREW KAISER on 3/6/24.
+////
 //
-//  ViewController.swift
-//  CLCsuperFanApp_Kaiser
+//import UIKit
+//import FirebaseCore
+//import FirebaseDatabase
+//import FirebaseAuth
 //
-//  Created by ANDREW KAISER on 3/6/24.
+////struct AppData {
+////    static var user: Student!
+////    static var masterUsers: [Student] = []
+////    static var masterCodes: [AccessCode] = []
+////    static var count = 0 
+////}
 //
-
-import UIKit
-import FirebaseCore
-import FirebaseDatabase
-import FirebaseAuth
-
-struct AppData {
-    static var user: Student!
-    static var masterUsers: [Student] = []
-    static var masterCodes: [AccessCode] = []
-    static var count = 0 
-}
-
-class ViewController: UIViewController {
-    
-    var ref: DatabaseReference!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        ref = Database.database().reference()
-        
-        // SETTING MASTER STU ARRAY
-        ref.child("Users").observe(.childAdded, with: { (snapshot) in
-            let dict = snapshot.value as! [String:Any]
-            let u = Student(dict: dict)
-            u.firebaseKey = snapshot.key
-            AppData.masterUsers.append(u)
-        })
-        
-        // called after .childAdded
-        ref.child("Users").observeSingleEvent(of: .value, with: { snapshot in
-                print("--inital load has completed and the last user was read--")
-            // sorts
-            AppData.masterUsers.sort(by: {$0.points > $1.points })
-            for i in 0..<AppData.masterUsers.count {
-                AppData.masterUsers[i].declareRank(i)
-                print("Username: \(AppData.masterUsers[i].username) Rank: #\(AppData.masterUsers[i].globalRank)")
-            }
-        })
-        // sorts the array in decending order based on points
-        
-        // SETTING UP MASTER CODE ARRAY
-        ref.child("Codes").observe(.childAdded, with: { (snapshot) in
-            let dict = snapshot.value as! [String:Any]
-            let c = AccessCode(dict: dict)
-            c.firebaseKey = snapshot.key
-            AppData.masterCodes.append(c)
-        })
-        
-        // called after .childAdded
-        ref.child("Codes").observeSingleEvent(of: .value, with: { snapshot in
-                print("--inital load has completed and the last user was read--")
-        })
-    }
-    
-    @IBAction func loginAction(_ sender: Any) {
-        performSegue(withIdentifier: "toLogin", sender: self)
-    }
-    
-    @IBAction func createAction(_ sender: Any) {
-        performSegue(withIdentifier: "toCreate", sender: self)
-
-    }
-    
-    
-
-}
-
+//class ViewController: UIViewController {
+//    
+//    var ref: DatabaseReference!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        ref = Database.database().reference()
+//        
+//        // SETTING MASTER STU ARRAY
+//        ref.child("Users").observe(.childAdded, with: { (snapshot) in
+//            let dict = snapshot.value as! [String:Any]
+//            let u = Student(dict: dict)
+//            u.firebaseKey = snapshot.key
+//            AppData.masterUsers.append(u)
+//        })
+//        
+//        // called after .childAdded
+//        ref.child("Users").observeSingleEvent(of: .value, with: { snapshot in
+//                print("--inital load has completed and the last user was read--")
+//            // sorts
+//            AppData.masterUsers.sort(by: {$0.points > $1.points })
+//            for i in 0..<AppData.masterUsers.count {
+//                AppData.masterUsers[i].declareRank(i)
+//                print("Username: \(AppData.masterUsers[i].username) Rank: #\(AppData.masterUsers[i].globalRank)")
+//            }
+//        })
+//        // sorts the array in decending order based on points
+//        
+//        // SETTING UP MASTER CODE ARRAY
+//        ref.child("Codes").observe(.childAdded, with: { (snapshot) in
+//            let dict = snapshot.value as! [String:Any]
+//            let c = AccessCode(dict: dict)
+//            c.firebaseKey = snapshot.key
+//            AppData.masterCodes.append(c)
+//        })
+//        
+//        // called after .childAdded
+//        ref.child("Codes").observeSingleEvent(of: .value, with: { snapshot in
+//                print("--inital load has completed and the last user was read--")
+//        })
+//    }
+//    
+//    @IBAction func loginAction(_ sender: Any) {
+//        performSegue(withIdentifier: "toLogin", sender: self)
+//    }
+//    
+//    @IBAction func createAction(_ sender: Any) {
+//        performSegue(withIdentifier: "toCreate", sender: self)
+//
+//    }
+//    
+//    
+//
+//}
+//
